65be61c9f65ae557a015e20253bf2c9149f3b449
Azuma-yutaro <azuma.yutaro1126@gmail.com>
2025-11-23 17:12:25 +0900

    ✨ クイズスタート画面

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -7,10 +7,27 @@ use App\Models\Category;
 
 class PlayController extends Controller
 {
+    // プレイヤー画面TOPページ
     public function top(){
         $categories = Category::all();
         return view('play.top',[
             'categories' => $categories
         ]);
     }
+
+
+    // クイズ画面スタート画面表示
+    public function categories(Request $request,int $categoryId){
+        // dd($request,$categoryId);
+        $category = Category::withCount('quizzes')->findOrFail($categoryId);
+
+        // dd($category->quizzes_count);
+
+        return view('play.start',
+        ['category'    => $category,
+        'quizzesCount' => $category->quizzes_count
+        ]);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,12 @@ use Illuminate\Support\Facades\Route;
 // プレイヤー画面
 Route::get('/', [PlayController::class,'top'])->name('top');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// クイズスタート画面
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('categories/{categoryId}',[PlayController::class,'categories'])->name('categories');
 
+
+// 管理者認証機能
 require __DIR__.'/auth.php';
 
 
